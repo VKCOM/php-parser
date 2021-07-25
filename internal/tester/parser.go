77ede177250e0952c0bb9d3103c1c769d83318f1
Makhnev Petr <51853996+i582@users.noreply.github.com>
2021-07-25 10:45:32 +0300

    tester: added parser suite to tester package (#9)

---
+++ internal/tester/parser.go
@@ -0,0 +1,46 @@
+package tester
+
+import (
+	"testing"
+
+	"github.com/z7zmey/php-parser/pkg/ast"
+	"github.com/z7zmey/php-parser/pkg/conf"
+	"github.com/z7zmey/php-parser/pkg/parser"
+	"github.com/z7zmey/php-parser/pkg/version"
+	"gotest.tools/assert"
+)
+
+type ParserTestSuite struct {
+	t *testing.T
+
+	Code     string
+	Expected ast.Vertex
+
+	Version version.Version
+}
+
+func NewParserTestSuite(t *testing.T) *ParserTestSuite {
+	return &ParserTestSuite{
+		t: t,
+		Version: version.Version{
+			Major: 7,
+			Minor: 4,
+		},
+	}
+}
+
+func (p *ParserTestSuite) UsePHP8() {
+	p.Version = version.Version{Major: 8, Minor: 0}
+}
+
+func (p *ParserTestSuite) Run() {
+	config := conf.Config{
+		Version: &p.Version,
+	}
+
+	actual, err := parser.Parse([]byte(p.Code), config)
+	if err != nil {
+		p.t.Fatalf("Error parse: %v", err)
+	}
+	assert.DeepEqual(p.t, p.Expected, actual)
+}

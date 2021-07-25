53554e95dad7c21ff65b302de39a2246ca31e273
i582 <i58215@voenmeh.ru>
2021-07-25 16:55:06 +0300

    php8: added ParserErrorTestSuite for testing parser errors

---
+++ internal/tester/parser_error.go
@@ -0,0 +1,52 @@
+package tester
+
+import (
+	"testing"
+
+	"github.com/z7zmey/php-parser/pkg/conf"
+	"github.com/z7zmey/php-parser/pkg/errors"
+	"github.com/z7zmey/php-parser/pkg/parser"
+	"github.com/z7zmey/php-parser/pkg/version"
+	"gotest.tools/assert"
+)
+
+type ParserErrorTestSuite struct {
+	t *testing.T
+
+	Code     string
+	Expected []*errors.Error
+
+	Version version.Version
+}
+
+func NewParserErrorTestSuite(t *testing.T) *ParserErrorTestSuite {
+	return &ParserErrorTestSuite{
+		t: t,
+		Version: version.Version{
+			Major: 7,
+			Minor: 4,
+		},
+	}
+}
+
+func (p *ParserErrorTestSuite) UsePHP8() {
+	p.Version = version.Version{Major: 8, Minor: 0}
+}
+
+func (p *ParserErrorTestSuite) Run() {
+	config := conf.Config{
+		Version: &p.Version,
+	}
+
+	var errs []*errors.Error
+
+	config.ErrorHandlerFunc = func(e *errors.Error) {
+		errs = append(errs, e)
+	}
+
+	_, err := parser.Parse([]byte(p.Code), config)
+	if err != nil {
+		p.t.Fatalf("Error parse: %v", err)
+	}
+	assert.DeepEqual(p.t, p.Expected, errs)
+}

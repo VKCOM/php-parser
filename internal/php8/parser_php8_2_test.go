e0409192c5730e850bb48ef510d5d3cf0701e624
i582 <51853996+i582@users.noreply.github.com>
2022-06-26 01:11:42 +0300

    php8.2: added readonly classes support

---
+++ internal/php8/parser_php8_2_test.go
@@ -0,0 +1,59 @@
+package php8_test
+
+import (
+	"testing"
+
+	"github.com/VKCOM/php-parser/internal/tester"
+)
+
+func TestClassReadonlyModifier(t *testing.T) {
+	suite := tester.NewParserDumpTestSuite(t)
+	suite.UsePHP8()
+	suite.Code = `<?php
+readonly class Foo {
+	public string $a;
+}
+`
+
+	suite.Expected = `&ast.Root{
+	Stmts: []ast.Vertex{
+		&ast.StmtClass{
+			Modifiers: []ast.Vertex{
+				&ast.Identifier{
+					Val: []byte("readonly"),
+				},
+			},
+			Name: &ast.Identifier{
+				Val: []byte("Foo"),
+			},
+			Stmts: []ast.Vertex{
+				&ast.StmtPropertyList{
+					Modifiers: []ast.Vertex{
+						&ast.Identifier{
+							Val: []byte("public"),
+						},
+					},
+					Type: &ast.Name{
+						Parts: []ast.Vertex{
+							&ast.NamePart{
+								Val: []byte("string"),
+							},
+						},
+					},
+					Props: []ast.Vertex{
+						&ast.StmtProperty{
+							Var: &ast.ExprVariable{
+								Name: &ast.Identifier{
+									Val: []byte("$a"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+},`
+
+	suite.Run()
+}

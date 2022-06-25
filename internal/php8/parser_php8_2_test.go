945581a3a0d080d3492e55cea2b957ea8ee81fdf
i582 <51853996+i582@users.noreply.github.com>
2022-06-26 01:16:00 +0300

    more tests

--- internal/php8/parser_php8_2_test.go
+++ internal/php8/parser_php8_2_test.go
@@ -13,6 +13,10 @@ func TestClassReadonlyModifier(t *testing.T) {
 readonly class Foo {
 	public string $a;
 }
+
+final readonly class Boo {
+	public string $a;
+}
 `
 
 	suite.Expected = `&ast.Root{
@@ -25,6 +29,44 @@ readonly class Foo {
 			},
 			Name: &ast.Identifier{
 				Val: []byte("Foo"),
+			},
+			Stmts: []ast.Vertex{
+				&ast.StmtPropertyList{
+					Modifiers: []ast.Vertex{
+						&ast.Identifier{
+							Val: []byte("public"),
+						},
+					},
+					Type: &ast.Name{
+						Parts: []ast.Vertex{
+							&ast.NamePart{
+								Val: []byte("string"),
+							},
+						},
+					},
+					Props: []ast.Vertex{
+						&ast.StmtProperty{
+							Var: &ast.ExprVariable{
+								Name: &ast.Identifier{
+									Val: []byte("$a"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		&ast.StmtClass{
+			Modifiers: []ast.Vertex{
+				&ast.Identifier{
+					Val: []byte("final"),
+				},
+				&ast.Identifier{
+					Val: []byte("readonly"),
+				},
+			},
+			Name: &ast.Identifier{
+				Val: []byte("Boo"),
 			},
 			Stmts: []ast.Vertex{
 				&ast.StmtPropertyList{
